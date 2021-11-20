e331881a2b37ad8194e2852f82357e4a4386c411
detillierjody <detillierjody@gmail.com>
2021-11-20 16:02:27 -0600

    commit test - font changes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,8 +28,8 @@ void PFMProject0AudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
+    g.setColour (juce::Colours::green); //changed font color and size to test that everything is good for future commits/branches
+    g.setFont (35.0f);
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
